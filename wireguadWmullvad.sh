7ee7f81e282bc847ea2e4bf9f6e33aa17efad344
Chris <atnl@protonmail.com>
2020-04-05 15:53:09 -0400

    Add files via upload

---
+++ wireguadWmullvad.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install Mullvad with WireGuard on Debian 10 LINUX
+# 1st need to install the Debian dependency pkgs for WireGuard 
+ git clone https://salsa.debian.org/debian/wireguard-linux-compat.git
+
+# https://wiki.debian.org/Wireguard
+echo "deb http://deb.debian.org/debian/ unstable main" | sudo tee /etc/apt/sources.list.d/unstable-wireguard.list
+printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' | sudo tee /etc/apt/preferences.d/limit-unstable
+apt update
+apt install wireguard
+   
+wget --content-disposition https://mullvad.net/download/deb/latest
+sudo apt-get -y install gdebi-core && sudo gdebi MullvadVPN-2020.3_amd64.deb
+
+# Install WireGuard Network interface device
+ip link add dev wg0 type wireguard
+echo "assingn an IP including the range for wg0 : "
+read  IP
+ip address add dev wg0 $IP
+
+mullvad
+
+
+# Then, in the GUI app
+# regenrate keys for wireguard
+# and set wireguard as preferred tunnel protocol

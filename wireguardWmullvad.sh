27d1ae7ab55b8534ba89a511f383c34dd6b3dc5a
Chris <atnl@protonmail.com>
2020-10-21 18:31:07 -0400

    Update wireguardWmullvad.sh

--- wireguardWmullvad.sh
+++ wireguardWmullvad.sh
@@ -7,17 +7,16 @@
 # https://wiki.debian.org/Wireguard
 echo "deb http://deb.debian.org/debian/ unstable main" | sudo tee /etc/apt/sources.list.d/unstable-wireguard.list
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' | sudo tee /etc/apt/preferences.d/limit-unstable
-apt update
 apt install wireguard
 
 # Download & install Mullvad. This wget cmd works. Not the /deb/latest
-wget https://mullvad.net/media/app/MullvadVPN-2020.5_amd64.deb
-sudo apt-get -y install gdebi-core && sudo gdebi MullvadVPN-2020.5_amd64.deb
+wget https://mullvad.net/media/app/MullvadVPN-2020.6_amd64.deb
+sudo apt-get -y install gdebi-core && sudo gdebi MullvadVPN-2020.6_amd64.deb
 
 # Install WireGuard Network interface device
 # https://www.wireguard.com/quickstart/
 ip link add dev wg0 type wireguard
-echo "Example: 192.168.2.1/24"
+echo "Example: ***** 192.168.2.1/24 *****"
 echo "assingn an IP including the range for wg0 : "
 read  IP
 ip address add dev wg0 $IP
@@ -25,4 +24,6 @@ ip address add dev wg0 $IP
 #Start Mullvad
 mullvad
 
-# And set wireguard as preferred tunnel protocol.
+#Set Wireguard as preferred tunnel protocol.
+# If you cannot connect then set the WG port to 53
+# And put in bridge mode

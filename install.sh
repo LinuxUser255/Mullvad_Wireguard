3483e1028ee89b8f5bd8005f9ecb57878c2ba4a4
Chris <atnl@protonmail.com>
2022-03-02 18:32:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,15 @@ gpg --import mullvad-code-signing.asc
 
 # Follow the directions on screen
 echo ""
-printf "Trust the signing key: Run gpg> trust\n\n"
-printf "To open the GnuPG key edit prompt:\n\n"
-printf "Run gpg> trust\n\n"
-printf "'Enter '5' and then 'yes'.\n\n"
-printf "Set trust level, Enter '5' and then 'yes'\n\n"
-printf "to quit press q\n\n"
-printf "the gpg --edit-key option will now run.\n\n"
+echo ""
+printf "\e[1;31m Trust the signing key: Run gpg> trust \e[0m\n\n"
+printf "\e[1;31m To open the GnuPG key edit prompt: \e[0m\n\n"
+printf "\e[1;31m Run gpg> trust \e[0m\n\n"
+printf "\e[1;31m 'Enter '5' and then 'yes'. \e[0m\n\n"
+printf "\e[1;31m Set trust level, Enter '5' and then 'yes' \e[0m\n\n"
+printf "\e[1;31m to quit press q \e[0m\n\n"
+printf "\e[1;31m the gpg --edit-key option will now run. \e[0m\n\n"
+
 
 gpg --edit-key A1198702FC3E0A09A9AE5B75D5A1D4F266DE8DDF
 echo ""
@@ -30,5 +32,21 @@ wget --trust-server-names https://mullvad.net/download/app/deb/latest/signature
 
 # Verify the signature
 gpg --verify MullvadVPN-"${latest}".deb.asc
+
+printf "\e[1;35m ------------------------------------------------------------- \e[0m\n"
+echo -e  "\e[1;31m Proceed with package installation? (y/N). \e[0m\n"
+read sel
+
+if [[ $sel == "y"  ]]
+then
+sudo dpkg -i MullvadVPN-*amd64.deb
+
+
+elif [[ $sel != "y" ]]
+then    
+echo "Installation canceled"
+
+
+fi
 
 
